0788caa8706256697b4035172afb2536c5693164
david rolfs and guy anderson <guy@>
2017-04-27 15:39:10 -0700

    create branch to alert type of triangle

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,24 @@
 $(document).ready(function(){
   $("form#triangle").submit(function(){
-    alert("clicked")
+    var side1 = parseInt($("#side1").val());
+    var side2 = parseInt($("#side2").val());
+    var side3 = parseInt($("#side3").val());
 
 
-  })
 
+    if(side1+side2 <= side3 || side2+side3 <= side1 || side1+side3 <= side2){
+      alert("not a triangle");
+    } else{
+        if(side1 === side2 && side1 === side3){
+          alert("Equilateral")
+        }
+        else if(side1===side2 || side1===side3 || side2===side3){
+          alert("Isosceles");
+        }
+        else if(side1 !==side2 && side1 !== side3 && side2 !== side3){
+          alert("Scalene")
+        }
+      }
 
-
+  });
 })
