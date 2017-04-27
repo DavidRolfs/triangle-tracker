b9470c85b1e2590a05714a0e8d41b57b7fc40c90
david rolfs and guy anderson <david@>
2017-04-27 15:51:19 -0700

    add alert to notfy if form is left blank, or not a number

--- js/scripts.js
+++ js/scripts.js
@@ -6,20 +6,20 @@ $(document).ready(function(){
     var side3 = parseInt($("#side3").val());
 
 
-
-    if(side1+side2 <= side3 || side2+side3 <= side1 || side1+side3 <= side2){
-      $("#triangleType").text("not a triangle");
+    if(!side1 || !side2 || !side3){
+      alert("enter numbers!!")
     } else{
-        if(side1 === side2 && side1 === side3){
-          $("#triangleType").text("Equilateral")
+        if(side1+side2 <= side3 || side2+side3 <= side1 || side1+side3 <= side2){
+          $("#triangleType").text("not a triangle");
+        } else{
+            if(side1 === side2 && side1 === side3){
+              $("#triangleType").text("Equilateral")
+            } else if(side1===side2 || side1===side3 || side2===side3){
+              $("#triangleType").text("Isosceles");
+            } else if(side1 !==side2 && side1 !== side3 && side2 !== side3){
+              $("#triangleType").text("Scalene")
+            }
+          }
         }
-        else if(side1===side2 || side1===side3 || side2===side3){
-          $("#triangleType").text("Isosceles");
-        }
-        else if(side1 !==side2 && side1 !== side3 && side2 !== side3){
-          $("#triangleType").text("Scalene")
-        }
-      }
-
   });
 })
